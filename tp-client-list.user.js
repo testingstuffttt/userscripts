eb23850a88d686ed0f3798d729a5335d693e2092
Eskimo <43651677+Eskimonian@users.noreply.github.com>
2018-10-17 12:59:37 -0500

    Update tp-client-list.user.js

--- tp-client-list.user.js
+++ tp-client-list.user.js
@@ -3,8 +3,8 @@
 // @author        GSHR
 // @match      *://civicplus.tpondemand.com/*
 // @require http://code.jquery.com/jquery-3.3.1.min.js
-// @version        1.0
-// @downloadURL      https://gist.github.com/TheEskimonian/3e94bb640294816719628c89b519a1ac/raw/tp-client-list.user.js
+// @version        1.1.0
+// @downloadURL      https://github.com/Eskimonian/userscripts/raw/master/tp-client-list.user.js
 // @description        TP Client Name List
 // @run-at      document-end
 // ==/UserScript==
